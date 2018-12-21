5422e3ae6c7213b8c776e1337d9294214039afa2
IMlame <45635779+IMlame@users.noreply.github.com>
2018-12-21 15:15:58 -0800

    more neato

--- HelloGithub/src/main.java
+++ HelloGithub/src/main.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.awt.AWTException;
+import java.awt.Robot;
 
 public class main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws AWTException {
 		Scanner in = new Scanner(System.in);
 		System.out.println("type in first set of coord point");
 		Point point1 = new Point(in.nextInt(), in.nextInt());
@@ -9,52 +11,30 @@ public class main {
 		Point point2 = new Point(in.nextInt(), in.nextInt());
 		point1.getMethods();
 		while (1 == 1) {
+			Robot delay = new Robot();
 			System.out.println("method?");
 			Scanner moo = new Scanner(System.in);
 			String input = moo.nextLine();
-			if (input.indexOf("(") > -1 && input.indexOf(".") > -1) {
-				String point = input.substring(0, input.indexOf("."));
-				String method = input.substring(input.indexOf(".") + 1, input.indexOf("("));
-				if (point.equalsIgnoreCase("point1")) {
-					if (method.equals("getX")) {
-						System.out.println(point1.getX());
-					} else if (method.equals("getY")) {
-						System.out.println(point1.getY());
-					} else if (method.equals("distanceTo")) {
-						System.out.println(point1.toDistance(point2));
-					} else if (method.equals("midpointTo")) {
-						System.out.println(point1.midpointTo(point2));
-					} else if (method.equals("slopeTo")) {
-						System.out.println(point1.slopeTo(point2));
-					} else if (method.equals("quadrant")) {
-						System.out.println(point1.quadrant());
-					} else if (method.equals("getCoords")) {
-						System.out.println(point1.getCoords());
-					} else {
-						System.out.println("invalid method");
-					}
-				} else if (point.equalsIgnoreCase("point2")) {
-					if (method.equals("getX")) {
-						System.out.println(point2.getX());
-					} else if (method.equals("getY")) {
-						System.out.println(point2.getY());
-					} else if (method.equals("distanceTo")) {
-						System.out.println(point2.toDistance(point1));
-					} else if (method.equals("midpointTo")) {
-						System.out.println(point2.midpointTo(point1));
-					} else if (method.equals("slopeTo")) {
-						System.out.println(point2.slopeTo(point1));
-					} else if (method.equals("quadrant")) {
-						System.out.println(point2.quadrant());
-					} else if (method.equals("getCoords")) {
-						System.out.println(point2.getCoords());
-					} else {
-						System.out.println("invalid method");
-					}
-				} else {
-					System.out.println("invalid point");
+			String temp = "";
+				if(input.equals("1")) {
+					System.out.println("distance: " + point1.toDistance(point2));
 				}
-			}
+				if(input.equals("2")) {
+					System.out.println("midpoint: " + point1.midpointTo(point2));
+				}
+				if(input.equals("3")) {
+					System.out.println("slope: " + point1.slopeTo(point2));
+				}
+				if(input.equals("4")) {
+					System.out.println("point1 quad: " + point1.quadrant());
+					System.out.println("point2 quad: " + point2.quadrant());
+				}
+				if(input.equals("5")) {
+					System.out.println("point1: " + point1.getCoords());
+					System.out.println("point2: " + point2.getCoords());
+				}
+				delay.delay(2000);
+				Point.getMethods();
 		}
 
 	}
