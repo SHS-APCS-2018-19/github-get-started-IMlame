35e21e5e7fc1bb2fe9459e14f9a4adebc10ee405
IMlame <45635779+IMlame@users.noreply.github.com>
2019-01-30 18:44:53 -0800

    pog

--- HelloGithub/src/unit6/Exercise2.java
+++ HelloGithub/src/unit6/Exercise2.java
@@ -14,10 +14,42 @@ public class Exercise2 {
 		Scanner scan = new Scanner(file);
 
 		ArrayList<employee> list = new ArrayList<employee>();
-		while(scan.hasNextLine()) {
+		while (scan.hasNextLine()) {
 			list.add(employee.createEmployee(scan.nextLine()));
 		}
-		System.out.println(list.get(2));
+		employeeInfo(list);
+		employeeSkills(list);
+		employeeProjects(list);
+	}
+
+	public static void employeeInfo(ArrayList<employee> list) {
+		for (employee n : list) {
+			System.out.println("ID: " + n.getID() + " NAME: " + n.getName() + " BIRTHDAY: " + n.getBirthday());
+		}
+	}
+
+	public static void employeeSkills(ArrayList<employee> list) {
+		for (int i = 0; i < employee.uniqueSpecialty.size(); i++) {
+			System.out.println();
+			System.out.println(employee.uniqueSpecialty.get(i) + ": ");
+			for (employee n : list) {
+				if (n.Specialty.contains(employee.uniqueSpecialty.get(i))) {
+					System.out.println(n.getName());
+				}
+			}
+		}
+	}
+
+	public static void employeeProjects(ArrayList<employee> list) {
+		for (int i = 0; i < employee.uniqueProject.size(); i++) {
+			System.out.println();
+			System.out.println(employee.uniqueProject.get(i) + ": ");
+			for (employee n : list) {
+				if (n.Projects.contains(employee.uniqueProject.get(i))) {
+					System.out.println(n.getName());
+				}
+			}
+		}
 	}
 
 }
