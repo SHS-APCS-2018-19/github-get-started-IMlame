7a3eecbb83d79f6f7cf4601a0457b331f3c08648
IMlame <45635779+IMlame@users.noreply.github.com>
2019-01-30 18:44:21 -0800

    b1g brain

--- HelloGithub/src/unit6/employee.java
+++ HelloGithub/src/unit6/employee.java
@@ -3,12 +3,17 @@ package unit6;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class employee {
-	String id;
-	String name;
-	String birthday;
-	private static ArrayList<String> Specialty = new ArrayList<>();
-	private static ArrayList<String> Projects = new ArrayList<>();
+	private String id;
+	private String name;
+	private String birthday;
+	public static ArrayList<String> uniqueSpecialty = new ArrayList<>();
+	public static ArrayList<String> uniqueProject = new ArrayList<>();
+	public ArrayList<String> Specialty = new ArrayList<>();
+	public ArrayList<String> Projects = new ArrayList<>();
 	public static employee createEmployee(String info) {
 		Scanner three = new Scanner(info).useDelimiter(";");
 		employee newEmployee = new employee();
@@ -16,18 +21,37 @@ public class employee {
 		newEmployee.name = three.next();
 		newEmployee.birthday = three.next();
 		
-		Scanner scanSpecialty = new Scanner(three.next());
+		Scanner scanSpecialty = new Scanner(three.next()).useDelimiter(",");
 		while (scanSpecialty.hasNext()) {
-			Specialty.add(scanSpecialty.next());
+			String specialty = scanSpecialty.next();
+			if(!uniqueSpecialty.contains(specialty)) {
+				uniqueSpecialty.add(specialty);
+			}
+			newEmployee.Specialty.add(specialty);
 		}
 		
-		Scanner scanProject = new Scanner(three.next());
-		while (scanSpecialty.hasNext()) {
-			Projects.add(scanProject.next());
+		Scanner scanProject = new Scanner(three.next()).useDelimiter(",");
+		while (scanProject.hasNext()) {
+			String project = scanProject.next();
+			if(!uniqueProject.contains(project)) {
+				uniqueProject.add(project);
+			}
+			newEmployee.Projects.add(project);
 		}
 		return newEmployee;
 	}
-	public String toString() {
+	
+ 	public String toString() {
 		return id + " " + name + " " + birthday + " " + Specialty;
 	}
+ 	
+ 	public String getID() {
+ 		return id;
+ 	}
+ 	public String getName() {
+ 		return name;
+ 	}
+ 	public String getBirthday() {
+ 		return birthday;
+ 	}
 }
