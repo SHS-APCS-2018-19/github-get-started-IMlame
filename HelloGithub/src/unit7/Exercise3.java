7356564aa33298c0def6c602a3f5ac6a12a729d0
IMlame <45635779+IMlame@users.noreply.github.com>
2019-02-15 09:46:17 -0800

    exercise 3

--- HelloGithub/src/unit7/Exercise3.java
+++ HelloGithub/src/unit7/Exercise3.java
@@ -1,5 +1,30 @@
 package unit7;
 
-public class Exercise3 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Exercise3 {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner nums = new Scanner(new File("names.txt"));
+		int[] nameArray = new int[0];
+		while(nums.hasNextInt()) {
+			int[] nameTempArray = new int[nameArray.length+1];
+			nameTempArray = nameArray.clone();
+		}
+		selectionSort(numList);
+	}
+		public static void selectionSort(int[] a) {
+			for(int i = 0; i < a.length; i++) {
+				int min = i;
+				for(int j = i+1; j < a.length; j++) {
+					if(a[j] < a[min]) {
+						int temp = i;
+						a[i] = a[min];
+						a[min] = temp;
+					}
+				}
+			}
+		}
 }
