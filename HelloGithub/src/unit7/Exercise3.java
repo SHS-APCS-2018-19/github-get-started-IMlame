2e36ae2f93ce005ca49fc8f27d582bce3ad10ebd
IMlame <45635779+IMlame@users.noreply.github.com>
2019-02-25 08:43:04 -0800

    exercise 3

--- HelloGithub/src/unit7/Exercise3.java
+++ HelloGithub/src/unit7/Exercise3.java
@@ -1,30 +1,88 @@
-package unit7;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise3 {
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner nums = new Scanner(new File("names.txt"));
-		int[] nameArray = new int[0];
+		Scanner nums = new Scanner(new File("numbers.txt"));
+		ArrayList<Integer> intList = new ArrayList<>();
 		while(nums.hasNextInt()) {
-			int[] nameTempArray = new int[nameArray.length+1];
-			nameTempArray = nameArray.clone();
+			intList.add(nums.nextInt());
 		}
-		selectionSort(numList);
+		Integer[] intArray = new Integer[intList.size()];
+		intArray = intList.toArray(intArray);
+		selectionSort(intArray);
+		
+		
+		Scanner names = new Scanner(new File("names.txt"));
+		ArrayList<String> nameList = new ArrayList<>();
+		while(names.hasNext()) {
+			nameList.add(names.next());
+		}
+		String[] nameArray = new String[nameList.size()];
+		nameArray = nameList.toArray(nameArray);
+		selectionSortNamesAlpha(nameArray);
+		
+		while(true) {
+			System.out.println("1 for sorted int list \n2 for sorted name list\n3 for reverse sorted name list");
+			Scanner in = new Scanner(System.in);
+			int input = in.nextInt();
+			if(input == 1) {
+				System.out.println(Arrays.toString(intArray));
+			}
+			if(input == 2) {
+				selectionSortNamesAlpha(nameArray);
+				System.out.println(Arrays.toString(nameArray));
+			}
+			if(input == 3) {
+				selectionSortNamesRevAlpha(nameArray);
+				System.out.println(Arrays.toString(nameArray));
+			}
+		}
 	}
-		public static void selectionSort(int[] a) {
+		public static void selectionSort(Integer[] a) {
 			for(int i = 0; i < a.length; i++) {
 				int min = i;
 				for(int j = i+1; j < a.length; j++) {
 					if(a[j] < a[min]) {
-						int temp = i;
-						a[i] = a[min];
-						a[min] = temp;
+						min = j;
 					}
 				}
+				int temp = a[i];
+				a[i] = a[min];
+				a[min] = temp;
 			}
 		}
+		
+		public static void selectionSortNamesRevAlpha(String[] a) {
+			for(int i = 0; i < a.length; i++) {
+				int min = i;
+				for(int j = i+1; j < a.length; j++) {
+					if(a[j].compareTo(a[min]) > 0) {
+						min = j;
+					}
+				}
+				String temp = a[i];
+				a[i] = a[min];
+				a[min] = temp;
+			}
+		}
+		public static void selectionSortNamesAlpha(String[] a) {
+			for(int i = 0; i < a.length; i++) {
+				int min = i;
+				for(int j = i+1; j < a.length; j++) {
+					if(a[j].compareTo(a[min]) < 0) {
+						min = j;
+					}
+				}
+				String temp = a[i];
+				a[i] = a[min];
+				a[min] = temp;
+			}
+		}
+
+
 }
