9d887b4022b0fad898916d39377e2d54f5ea7bf8
IMlame <45635779+IMlame@users.noreply.github.com>
2019-03-04 09:28:34 -0800

    Update Exercise5.java

--- HelloGithub/src/unit8/Exercise5.java
+++ HelloGithub/src/unit8/Exercise5.java
@@ -1,5 +1,23 @@
-package unit8;
 
 public class Exercise5 {
+
+	public static void main(String[] args) {
+		int target = 13;
+		int[] arr = {1,3,5,7,10,13};
+		System.out.println(binarySearch(arr, target, 0, arr.length-1));
+	}
+	public static int binarySearch(int[] arr, int target, int min, int max) {
+		if(target > arr[max]) {
+			return -1;
+		} else {
+			if(target < arr[((min+max)/2)]) {
+				return(binarySearch(arr, target, min, (min+max)/2-1));
+			} else if(target > arr[((min+max)/2)]) {
+				return(binarySearch(arr, target, (min+max)/2+1, max));
+			} else {
+				return (min+max)/2;
+			}
+		}
+	}
 
 }
