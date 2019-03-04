975a3b164307d0fc99455bb1dc344e87369520da
IMlame <45635779+IMlame@users.noreply.github.com>
2019-03-04 09:28:54 -0800

    Update Recursion1.java

--- HelloGithub/src/unit8/Recursion1.java
+++ HelloGithub/src/unit8/Recursion1.java
@@ -1,5 +1,14 @@
-package unit8;
 
-public class Recursion1 {
+public class Recursionex1 {
+	public static void main(String[] args) {
+		System.out.println(reverse("SUSAN"));
+	}
 
+	public static String reverse(String rev) {
+		if(rev.length() <=1) {
+			return rev;
+		} else {
+			return reverse(rev.substring(1)) + rev.charAt(0);
+		}
+	}
 }
