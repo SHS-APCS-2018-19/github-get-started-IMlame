e799414040693bb383d4ca95d73f39cb8a4092c0
IMlame <45635779+IMlame@users.noreply.github.com>
2019-03-04 09:27:40 -0800

    Update Exercise2.java

--- HelloGithub/src/unit8/Exercise2.java
+++ HelloGithub/src/unit8/Exercise2.java
@@ -1,5 +1,16 @@
-package unit8;
+import java.util.Scanner;
 
 public class Exercise2 {
 
+	public static void main(String[] args) {
+		System.out.println(palindrome("rawwar"));
+	}
+	public static boolean palindrome(String input) {
+		if(input.length() <= 1) {
+			return true; 
+		} else {
+			return (input.substring(0, 1).equals(input.substring(input.length()-1)) && palindrome(input.substring(1, input.length()-1)));
+		}
+			
+	}
 }
