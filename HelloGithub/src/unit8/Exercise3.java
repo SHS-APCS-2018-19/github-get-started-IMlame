0ef93415b377b165f62559ab847d2e4180e2f4da
IMlame <45635779+IMlame@users.noreply.github.com>
2019-03-04 09:27:59 -0800

    Update Exercise3.java

--- HelloGithub/src/unit8/Exercise3.java
+++ HelloGithub/src/unit8/Exercise3.java
@@ -1,5 +1,16 @@
-package unit8;
 
 public class Exercise3 {
 
-}
+	public static void main(String[] args) {
+		System.out.println(fibonacci(4));
+	}
+
+	public static int fibonacci(int num) {
+		if (num <= 0) {
+			return 0;
+		} else if(num == 1) {
+			return 1;
+		} else {
+		return fibonacci(num - 1) + fibonacci(num - 2);
+	}
+}}
