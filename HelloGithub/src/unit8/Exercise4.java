7b9db017ae8448b78df694ff66eb0c7c4c3c054f
IMlame <45635779+IMlame@users.noreply.github.com>
2019-03-04 09:28:16 -0800

    Update Exercise4.java

--- HelloGithub/src/unit8/Exercise4.java
+++ HelloGithub/src/unit8/Exercise4.java
@@ -1,5 +1,14 @@
-package unit8;
 
 public class Exercise4 {
 
+	public static void main(String[] args) {
+		System.out.println(GCD(234024,324));
+	}
+	public static int GCD(int num1, int num2) {
+		if(num1 % num2 == 0) {
+			return num2;
+		} else {
+			return GCD(num2,num1%num2);
+		}
+	}
 }
